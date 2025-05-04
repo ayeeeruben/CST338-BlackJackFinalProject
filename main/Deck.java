adceb9b74ea691748fdb9f5576f8d53ae14887b6
ayeeeruben <marinez.ruben210@gmail.com>
2025-05-04 15:37:29 -0700

    generateDeck() implemented and card class changed to work with face cards

--- main/Deck.java
+++ main/Deck.java
@@ -4,12 +4,22 @@ import java.util.List;
 public class Deck {
     private Card[] cards = new Card [52];
     private int cardCount = 52; //this will decrement as we pull cards
+    private static final char[] suits = new char[] {'H','D','C','S'};
 
     public Deck() {
     }
 
-    public void generateDeck() { //return type???
-        //leaving it as void so it can compile
+    public void generateDeck(){
+        int j = 0;
+        for(int i = 0; i < suits.length; i++) {
+            for(int k = 0; k < 13; k++) {
+                cards[j] = new Card(k, suits[i]);
+                j++;
+            }
+        }
+
+    }
+    public void removeCard(Card card) {
 
     }
 
